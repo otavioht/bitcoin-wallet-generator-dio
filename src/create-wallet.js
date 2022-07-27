c19adf34957ecbbfbdb0867e4700e16bd0c8235f
Otavio Hara <otaviotsukamoto@gmail.com>
2022-07-26 22:34:23 -0300

    feat(core): wallet generator script
    Generates a wallet with a passphrase, address and private key

---
+++ src/create-wallet.js
@@ -0,0 +1,25 @@
+const bit32 = require('bip32')
+const bip39 = require('bip39')
+const bitcoin = require('bitcoinjs-lib')
+
+const network = bitcoin.networks.testnet
+
+const path = `m/49'/1'/0'/0`
+
+let mnemonic = bip39.generateMnemonic()
+const seed = bip39.mnemonicToSeedSync(mnemonic);
+
+let root = bit32.fromSeed(seed, network)
+
+let account = root.derivePath(path);
+let node = account.derive(0).derive(0);
+
+let btcAddress = bitcoin.payments.p2pkh({
+    pubkey: node.publicKey,
+    network: network
+}).address
+
+console.log("Carteira gerada")
+console.log("Endereço: " + btcAddress)
+console.log("Seed: " + mnemonic)
+console.log("Chave privada", node.toWIF())
